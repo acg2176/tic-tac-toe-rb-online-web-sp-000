251a0afa134075d9e8c2af507d5a1d1bf71bf2cf
Anna Guerrero <acg2176@columbia.edu>
2020-03-22 06:03:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -134,15 +134,15 @@ def winner(board)
   end
 end
 
-def play(board)
-  until over?(board) == true #while False
-    turn(board)
-  end
-  if won?(board) && winner(board) == "X"
-    puts "Congratulations X!"
-  elsif won?(board) && winner(board) == "O"
-      puts "Congratulations O!"
-  elsif draw?(board)
-    puts "Cat's Game!"
-  end
-end
+#def play(board)
+  #until over?(board) == true #while False
+    #turn(board)
+  #end
+  #if won?(board) && winner(board) == "X"
+    #puts "Congratulations X!"
+  #elsif won?(board) && winner(board) == "O"
+      #puts "Congratulations O!"
+  #elsif draw?(board)
+    #puts "Cat's Game!"
+  #end
+#end
